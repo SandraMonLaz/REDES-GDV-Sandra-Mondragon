573a95deb2750608e6e29d4a70ba56f2a661e5b2
Sandra <samondra@ucm.es>
2021-05-09 19:17:02 +0200

    Create Ejercicio7.cc

---
+++ Practica 2.1/Ejercicio7.cc
@@ -0,0 +1,98 @@
+#include <sys/types.h> 	//socket creation
+#include <sys/socket.h> //socket creation
+
+#include <netdb.h>	//socket AF_INET
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
+#include <thread>
+
+#define BUFFER 80
+using namespace std;
+
+class ThreadMessageTCP {
+public:
+    ThreadMessageTCP(int socket): sc(socket){}
+    ~ThreadMessageTCP(){}
+
+    char message(){
+            while(true){
+        char buffer[BUFFER + 1]; 
+
+        //Recieve data from the client
+        int bytes = recv(clientSd, (void *) buffer, BUFFER,0);
+
+		buffer[bytes] ='\0';//Last byte recieve
+
+        if( bytes == -1 ){
+            cout << "Error: While Reciving from Client \n";
+		    return -1;
+        }
+        if( bytes == 0 || buffer[0] == 'Q'){
+            cout << "End of conection \n";
+            break;
+        }
+        send(clientSd,buffer,bytes,0);
+    }
+    }
+private:
+    int sc;
+};
+/*
+ * args[0] -> P2.1_Ejericio1
+ * args[1] -> host
+ * args[2] -> port
+ * */
+int main(int argc, char* args[]){
+	//To check if everything is okay
+	int error = 0; 
+	//-------------Configuration----------------
+  	struct addrinfo hints;
+	struct addrinfo* res;
+
+    memset((void*) &hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	error = getaddrinfo(args[1], args[2], &hints, &res);
+	if(error != 0){
+		cout << "Error: Get Address Info \n";
+		return -1;
+	}
+    //------------Conection Management-----------
+    int sc = socket(res->ai_family, res->ai_socktype,0);
+	if(sc == -1) 
+	{
+        cout << "Error: Creating Socket \n";
+		return -1;
+	}
+    bind(sc, res->ai_addr, res->ai_addrlen);
+    //-------------TCP Management-------------------
+    listen(sc, 16);
+     //-------------Loop-----------------------------
+    while(true){
+        struct sockaddr client;
+        socklen_t       clientSize = sizeof(struct sockaddr);
+
+        int clientSd = accept(sc,&client, &clientSize);
+
+        char host[NI_MAXHOST];
+        char serv[NI_MAXSERV];
+
+        getnameinfo(&client, clientSize, host, NI_MAXHOST, serv,NI_MAXSERV, NI_NUMERICHOST);
+        cout << "Conection from " << host << " " << serv << "\n";
+
+        //Make the thread
+        ThreadMessageTCP* ms = new ThreadMessageTCP(clientSd);
+        thread([&ms](){
+            ms->message();
+            delete ms;
+        }).detach();
+    }
+
+	close(sc);
+	freeaddrinfo(res);
+	return 0;
+}
+
+
